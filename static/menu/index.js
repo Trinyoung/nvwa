724d7446e81013ee6df0fb7340ea0daf88263993
trinyoung <trinyoung@163.com>
2020-11-18 21:41:14 +0800

    fix(editor): 修改编辑器

---
+++ static/menu/index.js
@@ -0,0 +1,32 @@
+export const menu = {
+  home: {
+    type: 1,
+    text: '首页',
+    active: true
+  },
+  articles: {
+    type: 2,
+    text: '文章',
+    active: true
+  },
+  types: {
+    type: 3,
+    text: '分类',
+    active: true
+  },
+  loggers: {
+    type: 4,
+    text: '日志',
+    active: false
+  },
+  series: {
+    type: 5,
+    text: '连载',
+    active: false
+  },
+  albumn: {
+    type: 6,
+    text: '相册',
+    active: false
+  }
+}
