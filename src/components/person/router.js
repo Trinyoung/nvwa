bc820f032b684dc79b83572658ba3f3f265a7dec
trinyoung <trinyoung@163.com>
2020-11-23 09:31:09 +0800

    fix(person): 修改个人主页

--- src/components/person/router.js
+++ src/components/person/router.js
@@ -1,26 +1,26 @@
 /*
  * @Author: your name
  * @Date: 2020-11-11 18:05:18
- * @LastEditTime: 2020-11-23 09:11:51
+ * @LastEditTime: 2020-11-23 09:30:40
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \nvwa\src\components\Home\router.js
  */
-import home from '@/components/person'
+import person from '@/components/person'
 import article from '@/components/person/article'
-import homePage from '@/components/person/home'
+import personPage from '@/components/person/home'
 import Loggers from '@/components/person/Loggers'
 import types from '@/components/person/types'
 import detail from '@/components/person/content'
 const obj = {
   path: '/person/:uid',
-  name: 'Home',
-  component: home,
+  name: 'person',
+  component: person,
   props: true,
   children: [ {
     path: '/',
     name: 'main',
-    component: homePage
+    component: personPage
   }, {
     path: 'articles',
     name: 'article',
