5f2597fed8f53d59ea5f28f73fe0655697844c3a
trinyoung <trinyoung@163.com>
2020-12-18 20:04:11 +0800

    fix(stastic): 添加统计模块

--- src/components/console_dashboard/stastic.vue
+++ src/components/console_dashboard/stastic.vue
@@ -1,17 +1,14 @@
 <!--
  * @Author: your name
  * @Date: 2020-12-17 10:39:34
- * @LastEditTime: 2020-12-17 10:50:21
+ * @LastEditTime: 2020-12-17 13:56:32
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \nvwa\src\components\console_dashboard\stastic.vue
 -->
 <template>
   <div class='wrapper'>
-    <div class="inner">
-      <div class='chart' id='chart'></div>
-      <div class="btm"></div>
-    </div>
+    <div class='chart' id='chart'></div>
   </div>
 </template>
 <script>
@@ -54,6 +51,50 @@ export default {
           }
         },
         series: [{
+          type: 'liquidFill',
+          name: '全国就业率', // 系列名称，用于tooltip的显示，legend 的图例筛选
+          radius: '62%', // 水球图的半径
+          center: ['50%', '60%'], // 水球图的中心（圆心）坐标，数组的第一项是横坐标，第二项是纵坐标
+          // 水填充图的形状 circle 默认圆形  rect 圆角矩形  triangle 三角形
+          // diamond 菱形  pin 水滴状 arrow 箭头状  还可以是svg的path
+          shape: 'circle',
+          phase: 0, // 波的相位弧度 不设置  默认自动
+          direction: 'right', // 波浪移动的速度  两个参数  left 从右往左 right 从左往右
+          outline: {
+            show: true,
+            borderDistance: 0, // 边框线与图表的距离 数字
+            itemStyle: {
+              opacity: 1, // 边框的透明度   默认为 1
+              borderWidth: 1, // 边框的宽度
+              shadowBlur: 1, // 边框的阴影范围 一旦设置了内外都有阴影
+              shadowColor: '#fff', // 边框的阴影颜色,
+              borderColor: '#41dcd8' // 边框颜色
+            }
+          },
+          // 图形样式
+          itemStyle: {
+            color: '#4077eb', // 水球显示的背景颜色
+            opacity: 0.5, // 波浪的透明度
+            shadowBlur: 10 // 波浪的阴影范围
+          },
+          backgroundStyle: {
+            color: '#407bf3', // 水球未到的背景颜色
+            opacity: 0.5
+          },
+          // 图形的高亮样式
+          emphasis: {
+            itemStyle: {
+              opacity: 0.8 // 鼠标经过波浪颜色的透明度
+            }
+          },
+          // 图形上的文本标签
+          label: {
+            fontSize: 55,
+            fontWeight: 400,
+            color: '#fff'
+          },
+          data: [0.62] // 系列中的数据内容数组
+        }, {
           type: 'liquidFill',
           name: '全国就业率', // 系列名称，用于tooltip的显示，legend 的图例筛选
           radius: '62%', // 水球图的半径
